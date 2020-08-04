4e01e735f3ddc56993e61ac2e037de32ec10a1e6
Rishav Jain <44065908+rishavjain4@users.noreply.github.com>
2020-08-04 23:59:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
+from textanal import *
 from lsi import *
 
 
-#with open("htmldoc.html") as fp:
-#    soup = BeautifulSoup(fp,features="html5lib")
-html_content = requests.get('http://home.iitj.ac.in/~mkumar/').text
-soup = BeautifulSoup(html_content,features="html5lib")
-#print(soup.prettify())
-
+website_c=requests.get('http://home.iitj.ac.in/~mkumar/').text
+print("website read")
+soup = BeautifulSoup(website_c,features="html5lib")
+for script in soup(["script", "style"]):
+    script.decompose()
+source_contents =(soup.get_text()).split()
+#source_contents =cleandata(soup.get_text()) #query document
+#print(doc)
+print("got contents on hand")
 h1 = open("hyperlinks.txt","w+")
 h = []
 f1 = open("faulty.txt" ,"w+")
@@ -24,8 +27,6 @@ for link in soup.find_all('a'):
         try:
             request = requests.get(link.get('href'))
             if request.status_code == 200 :
-                #h.write(str(counter))
-                #h.write(":")
                 h1.write(link.get('href'))
                 h1.write("\n")
                 h.append(link.get('href'))
@@ -36,45 +37,58 @@ for link in soup.find_all('a'):
         except IOError:
             #print(link.get('href'))
             #print("Not a real URL")
-            continue
-#f.close()
-#h.close()
+                f1.write(link.get('href'))
+                f1.write("\n")
+                f.append(link.get('href'))
 ##########################################
-'''
-with open('tables.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["SN","hyperlink", "hyperlink_text","hyperlink_content","hyperlink_tm"])
-file.close()
-
-
-file = open('hyperlinks.txt', 'r')
-
-while True:
-    # Get next line from file
-    line = file1.readline()
-    # if line is empty
-    # end of file is reached
-    if not line:
-        break
-    soup= BeautifulSoup(line.strip(),features="html5lib")
-    writer.writerow([counter, line, , soup.get_text()])
-
-file1.close()
-'''
+print("found all hyperlinks")
 file = open('hyp_con.txt', 'w+')
 contents = []
-
-
+print("file opened")
 for line in h :
-    html_content = requests.get(line).text
-    soup = BeautifulSoup(html_content,features="html5lib")
-    contents.append(soup.get_text().replace("\n"," "))
-
+    hyperlink_string = requests.get(line).text
+    soup = BeautifulSoup(hyperlink_string,features="html5lib")
+    for script in soup(["script", "style"]):
+        script.decompose()    # rip it out
+    c=soup.get_text()
+    c=c.replace("\n"," ").replace("  "," ")
+    contents.append(c)
+print("scrapped content")
 for content in contents :
     file.write(content)
-
-
 file.close()
+print("scrapped content to file")
+
+#sims = LSImodel(contents , source_contents)
+#sims = LDA(contents ,doc)
+#for i,hs,sim in enumerate(sims,h, start=1):
+#    print(i,hs,sim)
+
+clean_contents = cleandata(contents )
+print("cleaned data")
+dictionary , dtm = create_corpus(clean_contents)
+print("got dick da")
+sims = LSImodel(clean_contents ,source_contents , dictionary , dtm )
 
 
 
+for a,b in zip(h,sims) :
+    print(a , b)
+print('###############################################################')
+#array = jensen_shannon(source_contents,dtm)
+#print(array)
+f1.close()
+h1.close()
+'''
+print('Comparing Result:', sims[query_doc_tf_idf])
+
+f1.close()
+h1.close()
+
+tf_idf = gensim.models.TfidfModel(corpus)
+for doc in tfidf[corpus]:
+    print([[dictionary[id], np.around(freq, decimals=2)] for id, freq in doc])
+building index
+sims = gensim.similarities.Similarity('workdir/',tf_idf[corpus],
+                                        num_features=len(dictionary))
+'''
